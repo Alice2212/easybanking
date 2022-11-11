0b5e8539f09ca89d52ab703d4314d798752a277b
Grace229 <itiunggrace@gmail.com>
2022-11-11 15:00:22 +0100

    first-commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      'blue': '#2D314D',
+      'cyan': '#2BB7DA',
+      'green': '#31D35C',
+      'grayish-blue': '#F3F4F6',
+      'gray': '#9698A6',
+      'gray-light': '#FAFAFA',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
